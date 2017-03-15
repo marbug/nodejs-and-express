b5f81caecfa17a9f1d1ee24b8ed85d9a1f8bf379
Maryan Bahnyuk <marbuga@gmail.com>
2017-03-15 11:30:30 +0200

    add scripts

---
+++ scripts/use-bitbucket.bat
@@ -0,0 +1,5 @@
+git remote -v
+
+git remote set-url origin git@bitbucket.org:marbug/angular2-and-typescript-and-redux.git
+
+git remote -v
---
+++ scripts/use-github.bat
@@ -0,0 +1,5 @@
+git remote -v
+
+git remote set-url origin git@github.com:marbug/angular2-and-typescript-and-redux.git
+
+git remote -v

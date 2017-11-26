accb01a575811596765549761d40a4c0698bc683
Maryan Bahnyuk <marbuga@gmail.com>
2017-11-26 12:43:10 +0200

    add scripts

---
+++ scripts/use-bitbucket.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git remote -v
+git remote set-url origin git@bitbucket.org:marbug/nodejs-and-express.git
+git remote -v
---
+++ scripts/use-github.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git remote -v
+git remote set-url origin git@github.com:marbug/nodejs-and-express.git
+git remote -v
